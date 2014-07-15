080b8c9a9034b73513ffa5988144bf27fbe1e0df
vũ văn tùng <mrvvt93@gmail.com>
2014-07-15 23:25:39 +0700

    second commit

---
+++ place2shot/src/tungtiep/place2shot/SignupActivity.java
@@ -0,0 +1,42 @@
+package tungtiep.place2shot;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class SignupActivity extends Activity implements OnClickListener {
+	
+	
+	private Button btsubmit;
+	Bundle bd;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	
+		setContentView(R.layout.signup);
+		
+		
+		btsubmit = (Button)findViewById(R.id.btSubmit);
+		btsubmit.setOnClickListener(this);
+		
+		}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+//		Intent back = new Intent("android.intent.action.MAIN");
+//		startActivity(back);
+		finish();
+	};
+	}
+	
+	
+	
+	
+
+
